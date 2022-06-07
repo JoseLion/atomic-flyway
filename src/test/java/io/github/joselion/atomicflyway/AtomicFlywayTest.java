b7c0afaa61f433b64bae10e3df44b8b56019fa23
Jose Luis Leon <joseluis5000l@gmail.com>
2022-06-07 12:56:58 -0500

    feat(core): Configurable API (#6)

--- src/test/java/io/github/joselion/atomicflyway/AtomicFlywayTest.java
+++ src/test/java/io/github/joselion/atomicflyway/AtomicFlywayTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.flywaydb.core.Flyway;
 import org.h2.jdbc.JdbcSQLSyntaxErrorException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -16,18 +17,75 @@ import uk.org.webcompere.systemstubs.SystemStubs;
 
 @UnitTest class AtomicFlywayTest {
 
+  private static final String H2_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+
+  private static final String H2_USER = "sa";
+
+  private static final String H2_PASSWORD = "";
+
+  @Nested class configure {
+    @Nested class when_the_configurer_is_provided {
+      @Test void create_an_instance_with_the_provided_configuration() {
+        final var atomicFlyway = AtomicFlyway.configure(config -> config.baselineOnMigrate(true));
+
+        assertThat(atomicFlyway.getFlywayConfig()).isNotNull();
+        assertThat(atomicFlyway.getFlywayConfig().isBaselineOnMigrate()).isTrue();
+      }
+    }
+
+    @Nested class when_the_configurer_is_not_provided {
+      @Test void creates_an_instance_with_the_default_configuration() {
+        final var atomicFlyway = AtomicFlyway.configure();
+
+        assertThat(atomicFlyway.getFlywayConfig()).isNotNull();
+        assertThat(atomicFlyway.getFlywayConfig().isBaselineOnMigrate()).isFalse();
+      }
+    }
+  }
+
   @Nested class attach {
+
+    private final Flyway flyway = Flyway.configure()
+      .dataSource(H2_URL, H2_USER, H2_PASSWORD)
+      .load();
+
+    @BeforeEach void cleanup() {
+      flyway.clean();
+    }
+
+    @Nested class when_the_url_option_is_not_used {
+      @Test void uses_the_configurer_datasource() throws Exception {
+        var exitCode = SystemStubs.catchSystemExit(() ->
+          AtomicFlyway.configure(config -> config.dataSource(H2_URL, H2_USER, H2_PASSWORD))
+            .attach("--migrate")
+        );
+
+        assertThat(exitCode).isEqualTo(CommandLine.ExitCode.OK);
+      }
+    }
+
+    @Nested class when_the_url_option_is_used {
+      @Test void overrides_the_datasource_connection_with_the_passed_options() throws Exception {
+        var exitCode = SystemStubs.catchSystemExit(() ->
+          AtomicFlyway.configure(config -> config.dataSource(H2_URL, H2_USER, H2_PASSWORD))
+            .attach(
+              "--migrate",
+              "-url", "bad_url",
+              "-user", "bad_user",
+              "-password", "bad_password"
+            )
+        );
+
+        assertThat(exitCode).isEqualTo(CommandLine.ExitCode.USAGE);
+      }
+    }
+
     @Nested class migrate {
       @Nested class when_the_migrate_option_is_passed {
         @Test void runs_flyway_migrations() throws Exception {
-          final var flyway = Flyway.configure()
-            .dataSource("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "")
-            .load();
           final var connection = flyway.getConfiguration()
             .getDataSource()
             .getConnection();
-
-          flyway.clean();
 
           assertThatThrownBy(() ->
             connection
@@ -38,12 +96,13 @@ import uk.org.webcompere.systemstubs.SystemStubs;
           .hasMessageStartingWith("Table \"flyway_schema_history\" not found (this database is empty)");
 
           final var exitCode = SystemStubs.catchSystemExit(() ->
-            AtomicFlyway.attach(
-              "--migrate",
-              "-url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
-              "-user", "sa",
-              "-password", ""
-            )
+            AtomicFlyway.configure()
+              .attach(
+                "--migrate",
+                "-url", H2_URL,
+                "-user", H2_USER,
+                "-password", H2_PASSWORD
+              )
           );
 
           final var result = connection
@@ -61,20 +120,16 @@ import uk.org.webcompere.systemstubs.SystemStubs;
 
       @Nested class when_the_migration_options_is_not_passed {
         @Test void does_not_run_flyway_migrations() throws Exception {
-          final var flyway = Flyway.configure()
-            .dataSource("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "")
-            .load();
           final var connection = flyway.getConfiguration()
             .getDataSource()
             .getConnection();
 
-          flyway.clean();
-
-          AtomicFlyway.attach(
-            "-url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
-            "-user", "sa",
-            "-password", ""
-          );
+          AtomicFlyway.configure()
+            .attach(
+              "-url", H2_URL,
+              "-user", H2_USER,
+              "-password", H2_PASSWORD
+            );
 
           assertThatThrownBy(() ->
             connection
@@ -91,20 +146,20 @@ import uk.org.webcompere.systemstubs.SystemStubs;
       @Nested class when_the_undo_migration_option_is_passed {
         @Test void runs_undoLastMigration() throws Exception {
           final var flyway = Flyway.configure()
-            .dataSource("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "")
+            .dataSource(H2_URL, H2_USER, H2_PASSWORD)
             .javaMigrations(new V001CreateAccountTable(), new V002AddCreatedAtToAccount())
             .load();
 
-          flyway.clean();
           flyway.migrate();
 
           final var exitCode = SystemStubs.catchSystemExit(() ->
-            AtomicFlyway.attach(
-              "--undo-migration",
-              "-url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
-              "-user", "sa",
-              "-password", ""
-            )
+            AtomicFlyway.configure()
+              .attach(
+                "--undo-migration",
+                "-url", H2_URL,
+                "-user", H2_USER,
+                "-password", H2_PASSWORD
+              )
           );
 
           assertThat(exitCode).isEqualTo(CommandLine.ExitCode.OK);
